880522df57b0da17c52cfb1659a009d55aa2a31c
C VA <info@vanauseloos.be>
2019-04-16 14:50:39 +0200

    init commit

---
+++ src/main/java/be/vanauseloos/springactivitidemo/SpringActivitiDemoApplication.java
@@ -0,0 +1,13 @@
+package be.vanauseloos.springactivitidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActivitiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActivitiDemoApplication.class, args);
+	}
+
+}
